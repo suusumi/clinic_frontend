5e998f5aa244ad5d75ee1b87b69fe94e5956df71
Sergey Groshev <wer113wer@yandex.ru>
2024-10-21 13:18:48 +0300

    [feat](routes): add new path (admin panel)
    Now go to LoginPage, will write correct soon

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router-dom";
 import {MainPage} from "src/pages/main";
 import {DocumentsPage} from "src/pages/documents";
 import {PrivacyPolicyPage} from "src/pages/privacy-policy";
+import {LoginPage} from "src/pages/login";
 
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
     {
         path: "/privacy-policy",
         element: <PrivacyPolicyPage />
+    },
+    {
+        path: "/auth-admin",
+        element: <LoginPage/>   //  TODO: если залогинен -> на страницу админки
     }
     // TODO: добавить страницу записи
 ])
